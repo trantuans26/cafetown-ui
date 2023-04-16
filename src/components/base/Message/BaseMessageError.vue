9a54f25f3145bd2368766bbd320bf1bf6ac485c6
Tr Thai Tuan <tuanlinhtx02@gmail.com>
2023-04-16 15:48:35 +0700

    add: base error message

---
+++ src/components/base/Message/BaseMessageError.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="message--hasError" hidden>
+        <slot name="message"> {{text}} không được để trống </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["text"]
+}
+</script>
+
+<style>
+    .message--hasError {
+        position: absolute;
+        white-space: nowrap;
+        font-weight: normal;
+        bottom: -15px;
+        left: 0;
+        color: #ed4337;
+        z-index: 1;
+        font-size: 11px;
+        display: none;
+    }
+
+    .input--error +.message--hasError {
+        display: block;
+    }
+
+</style>
